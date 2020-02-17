01c7b5a96b6895b1a9313e2a5cf5fb66accbb400
yacoub gadban <yacoub.gadban@gmail.com>
2020-02-17 16:02:53 +0200

    update

--- src/app/components/main-page/main-page.component.ts
+++ src/app/components/main-page/main-page.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { MainService } from 'src/app/services/main.service';
 import { CouponList } from 'src/app/models/CouponList';
+import { CouponServiceService } from 'src/app/services/coupon-service.service';
+import { AnonymousSubject } from 'rxjs/internal/Subject';
 
 @Component({
   selector: 'app-main-page',
@@ -10,10 +12,39 @@ import { CouponList } from 'src/app/models/CouponList';
 export class MainPageComponent implements OnInit {
 
   
-  constructor(private service:MainService) { }
+  constructor(private mainService:MainService,private service:CouponServiceService) { }
   public couponList:CouponList[];
+  public couponList2=new CouponList();
+  
+ 
   ngOnInit() {
-  this.service.getAllCoupons().subscribe((data)=>{this.couponList=data});
-  }
+  this.mainService.getAllCoupons().subscribe((data)=>{this.couponList=data});
+  
+  
 
 }
+
+
+
+
+buyCoupon(couponList){
+  
+  this.couponList2=couponList;
+  this.mainService.UpdateCoupon(this.couponList2).subscribe((couponList)=>{couponList});
+  
+     console.log(couponList);
+     console.log(couponList['amount']);
+     console.log(this.couponList2)
+    
+    }
+    
+
+   }
+  
+ 
+ 
+  
+
+
+
+
